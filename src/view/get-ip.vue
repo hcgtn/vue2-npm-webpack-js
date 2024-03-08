69ab7a68c343ff01bd439a982d58ca321947ed10
hcgtn <1069256064@qq.com>
2024-03-08 10:23:26 +0800

    feature:新增实验

---
+++ src/view/get-ip.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="get-ip">
+        getIP
+    </div>
+</template>
+  
+<script>
+
+// import ip from 'ip';
+// import * as lite from 'caniuse-lite';
+import UAParser from 'ua-parser-js';
+
+export default {
+    name: 'GetIp',
+    components: {
+    },
+    props: {
+
+    },
+    mounted() {
+        // const ipAddress = ip.address();
+        // console.log('ipAddress>>>',ipAddress)
+        // console.log(lite.features['flexbox']);
+        // let f  = lite.features['flexbox'];
+        // console.log(lite.feature(f));
+
+        let parser = new UAParser();
+        let result = parser.getResult();
+        console.log(result.browser);
+        console.log(result.device);
+        console.log(result.os);
+        console.log(result.os.version);
+        console.log(result.engine.name);
+        console.log(result.cpu.architecture);
+        console.log(result);
+
+    },
+    methods: {
+
+    }
+}
+</script>
+  
+<style scoped></style>
+  
---
+++ src/view/npm-pack-test/index.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="npm-pack-test">
+        <JSONEditor class="jsoneditor-warp" @change="jsonEiditorChange" />
+    </div>
+</template>
+  
+<script>
+import JSONEditor from './JSONEditor';
+export default {
+    name: 'NpmTest',
+    components: {
+        JSONEditor
+    },
+    props: {
+
+    },
+    mounted() {
+
+    },
+    methods: {
+        jsonEiditorChange(v) {
+            console.log('jsonEiditorChange>>>>>', v)
+        }
+    }
+}
+</script>
+  
+<style scoped lang="less">
+.npm-pack-test {
+    .jsoneditor-warp{
+        height: 200px;
+        width:500px
+    }
+}
+</style>
+  
