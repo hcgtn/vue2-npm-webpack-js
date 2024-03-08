08433a2c34bdb3e53b8ba1fc466680f2a455d8ee
hcgtn <1069256064@qq.com>
2024-03-08 14:59:31 +0800

    eslint:test

--- src/main.js
+++ src/main.js
@@ -15,7 +15,7 @@ Vue.use(ViewUI);
 Vue.use(VXETable);
 // Vue.use(VueLazyload);
 // or with options
-const loadimage = require('./assets/logo.png')
+const loadimage = require('./assets/logo.png');
 const errorimage = require('./assets/logo.png');
 
 Vue.use(VueLazyload, {
